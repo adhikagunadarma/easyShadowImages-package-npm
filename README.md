9485c35287f4c294a9c9ca4cb61768ea29f0d38c
‘AdhikaGunadarma’ <adhikagunadarma@gmail.com; GIT_COMMITTER_NAME=AdhikaGunadarma; GIT_COMMITTER_EMAIL=adhikagunadarma@gmail.com>
2021-09-08 14:13:53 +0700

    first commit, includes whole packages and readme ready

---
+++ README.md
@@ -0,0 +1,27 @@
+# What is this?
+
+Update your images to get some shadows.
+Best tools used to work with non-designer.
+A customized reference source code from DesignCourse Tutorial 
+
+# Installation
+
+`npm i easyshadowimages --save`
+
+Then...
+
+```
+import {easyShadowImages} from easyshadowimages
+easyShadowImages({
+    shadow_weight : 'high',
+    shadow_padding : 'min'
+});
+
+```
+## Options
+
+Easy Shadow Images supports 2 options, both are which optional : 
+
+* *shadow_weight* - _high | medium | low_ (Defaults to medium)
+* *shadow_padding* - _max | none | min_ (Defaults to none)
+
---
+++ index.js
@@ -0,0 +1,53 @@
+function easyShadowImages(options){
+    let images = document.querySelectorAll('.shadow-images');
+    const shadowWeight = setupShadowWeight(options.shadow_weight);
+    const shadowPadding = setupShadowPadding(options.shadow_padding);
+    images.forEach((image) => {
+        image.style.boxShadow = `10px 10px ${shadowWeight} 1px rgba(0,0,0,0.12)`;
+        image.style.padding = shadowPadding
+    })
+   
+}
+
+function setupShadowWeight(type){
+    let shadowWeight = '0px'
+    switch(type){ // low, medium, high
+        case 'low' : 
+            shadowWeight = '10px'
+            break;
+
+        case 'high' : 
+            shadowWeight = '0px'
+            break;
+
+        case 'medium' : 
+            shadowWeight = '5px'
+            break;
+        
+        default : 
+            shadowWeight = '5px'
+            break;
+    }
+    return shadowWeight
+}
+
+
+function setupShadowPadding(type){
+    let shadowPadding = '0em'
+    switch(type){ // min, max , none
+        case 'min' : 
+            shadowPadding = '1em'
+            break;
+
+        case 'max' : 
+            shadowPadding = '3em'
+            break;
+  
+        default : 
+            shadowPadding = '0em'
+            break;
+    }
+    return shadowWeight
+}
+
+module.exports.easyshadowimages = easyShadowImages
